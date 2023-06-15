7727f3a13a7608ab0e24f9551d33015e7faf0ee3
Tomás Pellissari <tomaspellissaripavan@gmail.com>
2023-06-15 17:24:38 -0300

    Update openwriters.py (#6)

--- openwriters.py
+++ openwriters.py
@@ -1,55 +1,45 @@
 import openai
-import json
 
 # Define the functions for each feature
 
 def generate_prompt(theme):
     """Generate a writing prompt about the given theme."""
+    # TODO: Add logic to generate a writing prompt
     prompt = f"Write a story prompt about {theme}."
-    return json.dumps(prompt)
+    return prompt
+
+# Removed duplicated function definition
 
 def generate_character(role, genre):
     """Generate a character profile for the given role in the specified genre."""
+    # TODO: Add logic to generate a character
     character = {
         "role": role,
         "genre": genre,
         "name": "Character Name",
         "description": "Character description goes here."
     }
-    return json.dumps(character)
+    return character
 
-def generate_outline(genre, setting):
-    """Generate a story outline for the specified genre and setting."""
-    outline = {
-        "genre": genre,
-        "setting": setting,
-        "plot_points": ["Plot point 1", "Plot point 2", "Plot point 3"]
-    }
-    return json.dumps(outline)
+# ... (rest of your functions here, each with a TODO comment)
 
-def generate_dialogue(characters):
-    """Generate a dialogue between the given characters."""
-    dialogue = {
-        "characters": characters,
-        "content": "Dialogue content goes here."
-    }
-    return json.dumps(dialogue)
+def generate_scene(tone, characters):
+    """Generates a scene with a specific tone and characters."""
+    # First, we'll format the characters into a string to be used in the prompt.
+    characters_str = ', '.join([f'{char["role"]}: {char["name"]}' for char in characters])
 
-def get_writing_advice(topic):
-    """Provide writing advice on the given topic."""
-    advice = f"Writing advice on {topic}: Lorem ipsum dolor sit amet, consectetur adipiscing elit."
-    return json.dumps(advice)
+    # Now we generate a prompt for GPT-3 using the tone and characters.
+    prompt = f"Generate a {tone} scene with the following characters: {characters_str}."
+    
+    # We call GPT-3 to generate the scene.
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt=prompt,
+        temperature=0.6,
+        max_tokens=300
+    )
 
-def generate_ending(genre, resolution):
-    """Generate an ending for the specified genre with the given resolution."""
-    ending = {
-        "genre": genre,
-        "resolution": resolution,
-        "content": "Ending content goes here."
-    }
-    return json.dumps(ending)
-
-# Set up the conversation with the GPT model
+    return response.choices[0].text.strip()
 
 def run_conversation(input_messages):
     response = openai.ChatCompletion.create(
@@ -58,149 +48,32 @@ def run_conversation(input_messages):
     )
     return response
 
-# Handle the model's response and call subsequent functions if necessary
-
 def handle_function_call(message):
+    """Processes a function call, if any, and returns the result."""
     function_name = message["function_call"]["name"]
+    function_parameters = message["function_call"]["parameters"]
     function_response = None
 
+    try:
+        # A dictionary mapping function names to their corresponding functions
+        functions = {
+            "generate_prompt": generate_prompt,
+            # ...
+            "generate_character": generate_character,
+            # ...
+        }
 
-    if function_name == "generate_prompt":
-        function_response = generate_prompt("space exploration")
-        # Process the function_response as needed
+        # Fetch the function from the dictionary and call it
+        function_to_call = functions.get(function_name)
+        if function_to_call:
+            function_response = function_to_call(*function_parameters)
+        else:
+            print(f"Function {function_name} not found.")
 
-    elif function_name == "generate_character":
-        function_response = generate_character("villain", "fantasy")
-        # Process the function_response as needed
-
-    elif function_name == "generate_outline":
-        function_response = generate_outline("mystery", "Victorian London")
-        # Process the function_response as needed
-
-    elif function_name == "generate_dialogue":
-        function_response = generate_dialogue([{"role": "detective", "name": "Detective Name"},
-                                               {"role": "suspect", "name": "Suspect Name"}])
-        # Process the function_response as needed
-
-    elif function_name == "get_writing_advice":
-        function_response = get_writing_advice("suspenseful scenes")
-        # Process the function_response as needed
-
-    elif function_name == "generate_ending":
-        function_response = generate_ending("fantasy", "happy")
-
-
-        # Process the function_response as needed
-
-    
-
-    # Handle other function calls for remaining features...
+    except Exception as e:
+        print(f"Error while calling function {function_name}: {str(e)}")
 
     return function_response
-
-def handle_function_call(message):
-    function_name = message["function_call"]["name"]
-    function_response = None
-
-    if function_name == "generate_prompt":
-        function_response = generate_prompt("space exploration")
-        # Process the function_response as needed
-
-    elif function_name == "generate_character":
-        function_response = generate_character("villain", "fantasy")
-        # Process the function_response as needed
-
-    elif function_name == "generate_outline":
-        function_response = generate_outline("mystery", "Victorian London")
-        # Process the function_response as needed
-
-    elif function_name == "generate_dialogue":
-        function_response = generate_dialogue([{"role": "detective", "name": "Detective Name"},
-                                               {"role": "suspect", "name": "Suspect Name"}])
-        # Process the function_response as needed
-
-    elif function_name == "get_writing_advice":
-        function_response = get_writing_advice("suspenseful scenes")
-        # Process the function_response as needed
-
-    elif function_name == "generate_description":
-        function_response = generate_description("bustling city", "future")
-        # Process the function_response as needed
-
-    elif function_name == "suggest_improvements":
-        function_response = suggest_improvements("John was very very happy")
-        # Process the function_response as needed
-
-    # Handle other function calls for remaining features...
-    elif function_name == "generate_poem":
-        function_response = generate_poem("sonnet", "lost love")
-        # Process the function_response as needed
-
-    elif function_name == "analyze_themes":
-        function_response = analyze_themes("The Great Gatsby")
-        # Process the function_response as needed
-
-    elif function_name == "convert_genre":
-        function_response = convert_genre("sci-fi story", "fantasy")
-        # Process the function_response as needed
-
-    elif function_name == "generate_scene":
-        function_response = generate_scene("dramatic", [{"role": "friend", "name": "Friend 1"},
-                                                        {"role": "friend", "name": "Friend 2"}])
-        # Process the function_response as needed
-
-    elif function_name == "get_historical_context":
-        function_response = get_historical_context("Pride and Prejudice")
-        # Process the function_response as needed
-
-    elif function_name == "translate_text":
-        function_response = translate_text("Translate this poem to French", "French")
-        # Process the function_response as needed
-
-    elif function_name == "interpret_symbolism":
-        function_response = interpret_symbolism("The fallen leaves in her yard were like an orange sea, heralding the decay of summer.")
-        # Process the function_response as needed
-
-    elif function_name == "generate_metaphor":
-        function_response = generate_metaphor("life")
-        # Process the function_response as needed
-
-    elif function_name == "generate_story_starter":
-        function_response = generate_story_starter(["Harry Potter", "Lord of the Rings"])
-        # Process the function_response as needed
-
-    elif function_name == "generate_plot_prompt":
-        function_response = generate_plot_prompt("dystopian")
-        # Process the function_response as needed
-
-    elif function_name == "generate_blurb":
-        function_response = generate_blurb("romance", "time-traveling lovers")
-        # Process the function_response as needed
-
-    elif function_name == "generate_scenario":
-        function_response = generate_scenario(["Sherlock Holmes", "Tony Stark"])
-        # Process the function_response as needed
-
-    elif function_name == "generate_description":
-        function_response = generate_description("post-apocalyptic city")
-        # Process the function_response as needed
-
-    elif function_name == "describe_genre_elements":
-        function_response = describe_genre_elements("steampunk")
-        # Process the function_response as needed
-
-    elif function_name == "generate_character":
-        function_response = generate_character("Star Wars")
-        # Process the function_response as needed
-
-    elif function_name == "generate_ending":
-        function_response = generate_ending("heist", "twist")
-        # Process the function_response as needed
-
-    # Continue the conversation with the generated function response
-    return function_response
-
-# Run the conversation and access the generated output
 
 input_messages = [
     {"role": "system", "content": "You are a creative writing assistant."},
@@ -209,16 +82,25 @@ input_messages = [
 ]
 
 response = run_conversation(input_messages)
-message = response['choices'][0]['message']
 
-while message.get("function_call"):
-    function_response = handle_function_call(message)
+for i in range(10):  # limiting iterations to avoid potential infinite loop
+    message = response['choices'][0]['message']
+
+    if message.get("function_call"):
+        function_response = handle_function_call(message)
     
-    if function_response:
-        # Continue the conversation with the generated function response
-        input_messages.append({"role": "function", "name": message["function_call"]["name"], "content": function_response})
-        response = run_conversation(input_messages)
-        message = response['choices'][0]['message']
+        if function_response:
+            # Continue the conversation with the generated function response
+            input_messages.append({"role": "function", "name": message["function_call"]["name"], "content": function_response})
+            response = run_conversation(input_messages)
+        else:
+            break
+    else:
+        break
+
+    # Print output of each iteration
+    print("Iteration", i+1)
+    print(message['content'])
 
 output = message['content']
-print(output)
+print("Final output:", output)
